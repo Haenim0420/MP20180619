a3f8a356e47bbfdc1eddc4649ba5c2c9872cd4fa
haein <khiren80@gmail.com>
2018-06-19 09:58:46 +0900

    3
    3

---
+++ GoodJob3/ServerLogin/ServerLogin/MyPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ㅡㅛㅖㅁㅎㄷViewController.swift
+//  ServerLogin
+//
+//  Created by SWUCOMPUTER on 2018. 6. 19..
+//  Copyright © 2018년 Digital Media Dept. All rights reserved.
+//
+
+import UIKit
+
+class ______ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
